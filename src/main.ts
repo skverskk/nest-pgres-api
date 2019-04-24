8df3ec179b125644bb8eade1b4dbc00778a8e04b
skverskk <ken.skversky@gmail.com>
2019-04-24 17:54:22 -0400

    Setup Postgres Database and Entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server started on Port ${port}`, 'Bootstrap');
 }
 bootstrap();
