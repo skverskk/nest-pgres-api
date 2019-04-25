a5b05d26ee2d77acae26284ef7295d93afe97382
skverskk <ken.skversky@gmail.com>
2019-04-25 01:36:05 -0400

    Completed CRUD Operations - Postgres

---
+++ src/idea/idea.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { IdeaService } from './idea.service';
+import { IdeaDTO } from './idea.dto';
+
+@Controller('idea')
+export class IdeaController {
+  constructor(private ideaService: IdeaService) {}
+
+  @Get()
+  showAll() {
+    return this.ideaService.showAll();
+  }
+
+  @Get(':id')
+  readIdea(@Param('id') id: string) {
+    return this.ideaService.read(id);
+  }
+
+  @Post()
+  createIdea(@Body() data: IdeaDTO) {
+    return this.ideaService.create(data);
+  }
+
+  @Put(':id')
+  updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+    return this.ideaService.update(id, data);
+  }
+
+  @Delete(':id')
+  destroyIdea(@Param('id') id: string) {
+    return this.ideaService.destroy(id);
+  }
+}
